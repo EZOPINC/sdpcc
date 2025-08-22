3589c0d993dfc8a5f3c5d6ba4937e649a372ea70
utpal Das <ezoperationinc@gmail.com>
2025-08-22 17:19:22 -0600

    new  design updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,11 @@
 // Custom JS for SDPCC site
+// Mobile menu toggle
+  document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.getElementById("nav-links");
+
+    hamburger.addEventListener("click", function() {
+      navLinks.classList.toggle("open");
+      hamburger.classList.toggle("active");
+    });
+  });
